78a5974eb27039f25368f659b9a7597973a1fc92
MiSTER MiHH <mihneaturcu29@tuta.io>
2022-11-19 19:15:10 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	new file:   .vscode/settings.json
    	new file:   Service/service.py
    	new file:   Test/ai-test.py
    	modified:   main.py

---
+++ Service/service.py
@@ -0,0 +1,111 @@
+# Path: main.py
+#gym workout motion tracker
+
+import cv2
+import mediapipe as mp
+import time
+from enum import Enum
+
+import argparse
+
+# exercise enum
+class exercise_type(Enum):
+    SQUAT = 0
+    PUSHUP = 1
+
+
+
+
+
+def video_capture():
+    cap = cv2.VideoCapture(0)
+
+    mpPose = mp.solutions.pose
+    pose = mpPose.Pose()
+    mpDraw = mp.solutions.drawing_utils
+
+    pTime = 0
+
+    # check if the person is doin a push-up correctly 
+    def pushup_check(lankle, rankle, lknee, rknee, lhip, rhip):
+        # check if the knees are below the ankles
+        print("lankle: ", lankle)
+        if lknee[1] > lankle[1] and rknee[1] > rankle[1]:
+            # check if the hips are above the knees
+            if lhip[1] < lknee[1] and rhip[1] < rknee[1]:
+                return True
+
+        # (x,y,z) = lankle
+        # print(x,y,z)
+        return False
+
+
+    # check if the person is doin a squat correctly
+    def squat_check(lankle, rankle, lknee, rknee, lhip, rhip):
+        # check if the knees are below the ankles
+        if lknee[1] > lankle[1] and rknee[1] > rankle[1]:
+            # check if the hips are below the knees
+            if lhip[1] > lknee[1] and rhip[1] > rknee[1]:
+                return True
+        return False
+
+
+    nr_of_pushups = 0
+    prev_state="0"
+    state_stack=["0"]
+
+    while True:
+        success, img = cap.read()
+        imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        results = pose.process(imgRGB)
+        # print(results.pose_landmarks)
+        if results.pose_landmarks:
+            mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
+            # for id, lm in enumerate(results.pose_landmarks.landmark):
+            #     # print(id, lm)
+            #     h, w, c = img.shape
+            #     cx, cy = int(lm.x * w), int(lm.y * h)
+            #     print(id, cx, cy)
+            #     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+            # get the coordinates of the landmarks
+            lankle = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ANKLE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ANKLE].y]
+            rankle = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ANKLE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ANKLE].y]
+            lknee = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_KNEE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_KNEE].y]
+            rknee = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_KNEE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_KNEE].y]
+            lhip = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_HIP].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_HIP].y]
+            rhip = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_HIP].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_HIP].y]
+            # lshoulder = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_SHOULDER].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_SHOULDER].y]
+
+            print()
+
+            # check if the person is doin a squat correctly
+            state_stack.append(prev_state)
+            if pushup_check(lankle, rankle, lknee, rknee, lhip, rhip):
+                # print("Squatting correctly")
+
+                if min([0 if x!="correct" else 1 for x in state_stack[max(len(state_stack)-5,0):]])==1:
+                    nr_of_pushups+=1
+                prev_state="correct"
+            else:
+                # print("Squatting incorrectly")
+                prev_state="incorrect"
+            state_stack.append(prev_state)
+            while len(state_stack)>10:
+                state_stack.pop(0)
+            print([0 if x!="correct" else 1 for x in state_stack[max(len(state_stack)-5,0):]])
+            print("Nr of pushups: ", nr_of_pushups, state_stack)
+
+            # check if
+            if True: #debug
+                cTime = time.time()
+                fps = 1/(cTime-pTime)
+                pTime = cTime
+
+                cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
+                cv2.imshow("Image", img)
+                cv2.waitKey(1)
+
+
+
---
+++ Test/ai-test.py
@@ -0,0 +1,23 @@
+import Service as service 
+from enum import Enum 
+
+
+class exercise_type(Enum):
+    SQUAT = 0
+    PUSHUP = 1
+
+
+def main():
+    nr_of_pushups = 0
+    while True:
+        # service.update_service()
+        # push_stack=service.get_stack().get_exercise_stack(exercise_type.PUSHUP)
+        result=service.check_full_exercise(exercise_type.PUSHUP)
+        if result==0:
+            ++nr_of_pushups
+        print(nr_of_pushups)
+        time.sleep(0.0001)
+
+    #video_capture()
+
+main()
