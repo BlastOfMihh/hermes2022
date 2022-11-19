bc41479946b1c6b18671991490820730b15d8f23
MiSTER MiHH <mihneaturcu29@tuta.io>
2022-11-19 20:29:07 +0200

    Please enter the commit message for your changes. Lines starting  with '#' will be ignored, and an empty message aborts the commit.
     On branch main
     Your branch is up to date with 'origin/main'.
    
     Changes to be committed:
    	modified:   Service/service.py

--- Service/service.py
+++ Service/service.py
@@ -13,7 +13,46 @@ class exercise_type(Enum):
     SQUAT = 0
     PUSHUP = 1
 
+    # check if the person is doin a squat correctly
+def squat_check(lankle, rankle, lknee, rknee, lhip, rhip):
+    # check if the knees are below the ankles
+    if lknee[1] > lankle[1] and rknee[1] > rankle[1]:
+        # check if the hips are below the knees
+        if lhip[1] > lknee[1] and rhip[1] > rknee[1]:
+            return True
+    return False
 
+    # check if the person is doin a push-up correctly 
+def pushup_check(lankle, rankle, lknee, rknee, lhip, rhip):
+    # check if the knees are below the ankles
+    if lknee[1] > lankle[1] and rknee[1] > rankle[1]:
+        # check if the hips are above the knees
+        if lhip[1] < lknee[1] and rhip[1] < rknee[1]:
+            return True
+    # (x,y,z) = lankle
+    # print(x,y,z)
+    return False
+
+def show_video(img):
+    cv2.imshow("Image", img)
+    cv2.waitKey(1)
+
+def check_squat1(parts):
+    lankle = parts[0]
+    rankle = parts[1]
+    lknee = parts[2]
+    rknee = parts[3]
+    lhip = parts[4]
+    rhip = parts[5]
+    lwrist= parts[6]
+    rwrist= parts[7]
+    lshoulder = parts[8]
+    rshoulder = parts[9]
+    lelbow = parts[10]
+    relbow = parts[11]
+    if lelbow[1]<lshoulder[1] and relbow[1]<rshoulder[1]:
+        return True
+    return False 
 
 
 
@@ -25,87 +64,30 @@ def video_capture():
     mpDraw = mp.solutions.drawing_utils
 
     pTime = 0
-
-    # check if the person is doin a push-up correctly 
-    def pushup_check(lankle, rankle, lknee, rknee, lhip, rhip):
-        # check if the knees are below the ankles
-        print("lankle: ", lankle)
-        if lknee[1] > lankle[1] and rknee[1] > rankle[1]:
-            # check if the hips are above the knees
-            if lhip[1] < lknee[1] and rhip[1] < rknee[1]:
-                return True
-
-        # (x,y,z) = lankle
-        # print(x,y,z)
-        return False
-
-
-    # check if the person is doin a squat correctly
-    def squat_check(lankle, rankle, lknee, rknee, lhip, rhip):
-        # check if the knees are below the ankles
-        if lknee[1] > lankle[1] and rknee[1] > rankle[1]:
-            # check if the hips are below the knees
-            if lhip[1] > lknee[1] and rhip[1] > rknee[1]:
-                return True
-        return False
-
-
-    nr_of_pushups = 0
-    prev_state="0"
-    state_stack=["0"]
-
     while True:
         success, img = cap.read()
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         results = pose.process(imgRGB)
-        # print(results.pose_landmarks)
         if results.pose_landmarks:
             mpDraw.draw_landmarks(img, results.pose_landmarks, mpPose.POSE_CONNECTIONS)
-            # for id, lm in enumerate(results.pose_landmarks.landmark):
-            #     # print(id, lm)
-            #     h, w, c = img.shape
-            #     cx, cy = int(lm.x * w), int(lm.y * h)
-            #     print(id, cx, cy)
-            #     cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
-
-            # get the coordinates of the landmarks
-            lankle = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ANKLE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ANKLE].y]
-            rankle = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ANKLE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ANKLE].y]
-            lknee = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_KNEE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_KNEE].y]
-            rknee = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_KNEE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_KNEE].y]
-            lhip = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_HIP].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_HIP].y]
-            rhip = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_HIP].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_HIP].y]
-            # lshoulder = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_SHOULDER].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_SHOULDER].y]
-
-            print()
-
-            # check if the person is doin a squat correctly
-            state_stack.append(prev_state)
-            if pushup_check(lankle, rankle, lknee, rknee, lhip, rhip):
-                # print("Squatting correctly")
-
-                if min([0 if x!="correct" else 1 for x in state_stack[max(len(state_stack)-5,0):]])==1:
-                    nr_of_pushups+=1
-                prev_state="correct"
-            else:
-                # print("Squatting incorrectly")
-                prev_state="incorrect"
-            state_stack.append(prev_state)
-            while len(state_stack)>10:
-                state_stack.pop(0)
-            print([0 if x!="correct" else 1 for x in state_stack[max(len(state_stack)-5,0):]])
-            print("Nr of pushups: ", nr_of_pushups, state_stack)
-
-            # check if
-            if True: #debug
-                cTime = time.time()
-                fps = 1/(cTime-pTime)
-                pTime = cTime
-
-                cv2.putText(img, str(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
-
-                cv2.imshow("Image", img)
-                cv2.waitKey(1)
+            lankle    = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ANKLE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ANKLE].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ANKLE].z]
+            rankle    = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ANKLE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ANKLE].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ANKLE].z]
+            lknee     = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_KNEE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_KNEE].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_KNEE].z]
+            rknee     = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_KNEE].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_KNEE].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_KNEE].z]
+            lhip      = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_HIP].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_HIP].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_HIP].z]
+            rhip      = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_HIP].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_HIP].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_HIP].z]
+            lwrist    = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_WRIST].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_WRIST].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_WRIST].z]
+            rwrist    = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_WRIST].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_WRIST].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_WRIST].z]
+            lshoulder = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_SHOULDER].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_SHOULDER].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_SHOULDER].z]
+            rshoulder = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_SHOULDER].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_SHOULDER].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_SHOULDER].z]
+            lelbow    = [results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ELBOW].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ELBOW].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.LEFT_ELBOW].z]
+            relbow    = [results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ELBOW].x, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ELBOW].y, results.pose_landmarks.landmark[mpPose.PoseLandmark.RIGHT_ELBOW].z]
+            parts = [lankle, rankle, lknee, rknee, lhip, rhip, lwrist, rwrist, lshoulder, rshoulder, lelbow, relbow]
+            cTime = time.time()
+            if check_squat1(parts):
+                print(cTime,"Squat")
+            show_video(img)
 
 
 
+video_capture()
